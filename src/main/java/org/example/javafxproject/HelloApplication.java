6b55840f92dd09ba97685e7c21e0f8f9edce6f12
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-23 18:45:54 -0600

    Second commit

--- src/main/java/org/example/javafxproject/HelloApplication.java
+++ src/main/java/org/example/javafxproject/HelloApplication.java
@@ -3,6 +3,8 @@ package org.example.javafxproject;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -13,9 +15,23 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Color.BLACK);
 
         stage.setScene(scene);
+
+        stage.setTitle("Stage Demo Nahum");
+
+        stage.setWidth(420);
+        stage.setHeight(420);
+
+        stage.setResizable(false);
+
+        stage.setX(50);
+        stage.setY(50);
+
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("You can't escape unless you press q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
         stage.show();
     }
 }
